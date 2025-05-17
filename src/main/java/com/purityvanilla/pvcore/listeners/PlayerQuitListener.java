e9023c88884817d30c210747b1c08254cadbc45d
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-05-17 12:48:04 +0100

    Unload player data async from logout event

--- src/main/java/com/purityvanilla/pvcore/listeners/PlayerQuitListener.java
+++ src/main/java/com/purityvanilla/pvcore/listeners/PlayerQuitListener.java
@@ -22,14 +22,18 @@ public class PlayerQuitListener implements Listener {
 
         // Update player username and last seen
         plugin.getPlayerData().getPlayer(uuid).update(player.getName());
+        plugin.getServer().getAsyncScheduler().runNow(plugin, (task) -> {
+            unloadPlayerData(uuid, player.getName());
+        });
+    }
 
+    private void unloadPlayerData(UUID uuid, String name) {
         // Remove player data from cache and save to database
         plugin.getPlayerData().unloadPlayer(uuid);
         int unloadedLocations = plugin.getLocationData().unloadAllPlayerLocations(uuid);
         if (plugin.config().verbose()) {
-            plugin.getLogger().info(String.format("Player %s's data removed from cache", player.getName()));
-            plugin.getLogger().info(String.format("Unloaded %s of player %s's locations",
-                    unloadedLocations, player.getName()));
+            plugin.getLogger().info(String.format("Player %s's data removed from cache", name));
+            plugin.getLogger().info(String.format("Unloaded %s of player %s's locations", unloadedLocations, name));
         }
     }
 }
